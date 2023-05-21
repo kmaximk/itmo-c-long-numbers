9804bda33a77f6fc7d689fa5e583a051c40a42dc
Максим Колтаков <114956496+123herobrine@users.noreply.github.com>
2023-05-21 23:14:23 +0300

    Update LN.h

--- LN.h
+++ LN.h
@@ -5,37 +5,25 @@
 #ifndef LN_H
 #define LN_H
 #include <string_view>
-struct LN
+
+#include <cstdio>
+class LN
 {
-	int sign;
-	std::size_t capacity;
-	size_t size;
-	uint8_t *ar;
-	const size_t DEL = 1l << 8l;
-	int isNan;
-	explicit LN(const char *num);
+  public:
 	explicit LN(long long num = 0);
+	LN(LN &&ln) noexcept;
 	LN(const LN &x);
+	explicit LN(const char *num);
 	explicit LN(std::string_view num);
+	~LN();
 	LN &operator=(LN &&ln) noexcept;
 	LN operator~() const;
 	LN operator-();
-	LN(LN &&ln) noexcept;
 	LN &operator=(const LN &ln);
-	int subtractModule(const LN &ln, LN *ret) const;
 	LN operator/(const LN &ln) const;
 	LN operator%(const LN &ln) const;
 	LN operator-(const LN &ln) const;
-	void subtractSigned(LN &ret, const LN &ln) const;
 	LN operator*(const LN &ln) const;
-	void multiplySigned(LN &ret, const LN &ln) const;
-	LN operator+(const LN &ln) const;
-	void addSigned(LN &ret, const LN &ln) const;
-	int addModule(const LN &ln, LN *ret) const;
-	void divideSigned(LN &ret, const LN &ln) const;
-	int compareTo(const LN &ln) const;
-	int compareModule(const LN &ln) const;
-	void ensureCapacity();
 	LN &operator+=(const LN &ln);
 	LN &operator-=(const LN &ln);
 	LN &operator*=(const LN &ln);
@@ -49,12 +37,45 @@ struct LN
 	bool operator!=(const LN &ln) const;
 	explicit operator bool() const;
 	explicit operator long long() const;
+	LN operator+(const LN &ln) const;
 	char *toString() const;
+
+  private:
+	int sign_;
+	std::size_t capacity_;
+	size_t size_;
+	uint8_t *number_;
+	const size_t DEL_ = 1l << 8l;
+	int isNan_;
+	int subtractModule(const LN &ln, LN *ret) const;
+	void subtractSigned(LN &ret, const LN &ln) const;
+	void multiplySigned(LN &ret, const LN &ln) const;
+	void addSigned(LN &ret, const LN &ln) const;
+	int addModule(const LN &ln, LN *ret) const;
+	void divideSigned(LN &ret, const LN &ln) const;
+	int compareTo(const LN &ln) const;
+	int compareModule(const LN &ln) const;
+	void karatsubaMultiply(LN& ret, const LN& ln) const;
+	void ensureCapacity();
 	void resize(size_t newcap);
 	static int makeNum(char c);
 	static char makeChar(int num);
-	~LN();
+	static void lnReverse(LN& ln);
+	static void print(const LN &ln);
 };
 LN operator"" _ln(const char *ln);
+
+struct LNOverflowException {
+	const char* message;
+	explicit LNOverflowException(const char* string) {
+		this->message = string;
+	}
+};
+struct LNOutOfMemoryException {
+	const char* message;
+	explicit LNOutOfMemoryException(const char* string) {
+		this->message = string;
+	}
+};
 
 #endif	  // LN_H
